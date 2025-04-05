f9e07f6c538493eee3d451046c21fd93a2900462
Ajaodolapo1301 <thatdeveloperguy1301@gmail.com>
2025-04-05 08:17:16 +0100

    first

---
+++ server.js
@@ -0,0 +1,57 @@
+require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const errorHandler = require('./middleware/errorHandler');
+
+
+
+
+
+// Import routes
+const studentRoutes = require('./routes/studentRoutes');
+const teacherRoutes = require('./routes/teacherRoutes');
+const authRoutes = require('./routes/authRoutes');
+const courseRoutes = require('./routes/courseRoutes');
+const enrollmentRoutes = require('./routes/enrollmentRoutes');
+const availabilityRoutes = require('./routes/availabilityRoutes');
+
+
+const app = express();
+
+// Connect to database
+
+
+
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/students', studentRoutes);
+app.use('/api/teachers', teacherRoutes);
+app.use('/api/auth', authRoutes);
+app.use('/api/courses', courseRoutes);
+app.use('/api/enrollments', enrollmentRoutes);
+app.use('/api/availability', availabilityRoutes);
+
+// Basic route for testing
+app.get('/', (req, res) => {
+  res.send('School Management System API');
+});
+
+
+app.use(errorHandler);
+
+
+
+// Start server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
